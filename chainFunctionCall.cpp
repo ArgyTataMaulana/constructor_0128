12489fc7b541af8bff6c172127a8d9408ea6d51d
ArgyTataMaulana <halmaheru37@gmail.com>
2025-05-13 19:40:35 +0700

    membuat class buku dan int main dari konsep chainFunctioncall

---
+++ chainFunctionCall.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class buku {
+    string Judul;
+    public:
+    buku setJudul(string Judul){
+        this->Judul = Judul;
+        return *this;   //chain function
+    }
+    string getJudul(){
+        return this->Judul;
+    }
+};
+
+int main()
+{
+    buku bukunya;
+    //chain Function calls
+    cout << bukunya.setJudul("Matematika").getJudul();
+    return 0;
+}
